3e0b91da492c4bb0cd242a74b51b68330151563b
teguhfv <teguhfv@gmail.com>
2019-03-30 14:27:33 +0800

    link

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,14 +11,14 @@ const Header = (props) => (
         {/* </div> */}
         <div className="content">
             <div className="inner">
-                <h1>TEGUH F V</h1>
+                <h1>TEGUH FV</h1>
                 <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
                 for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Blog</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
