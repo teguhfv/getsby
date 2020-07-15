2edb387fdd25b0a1bc9c7ad6d6d4ef4a44d73b7c
teguhfv <teguhfv@gmail.com>
2020-07-16 03:34:10 +0800

    blog address

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,12 +10,12 @@ const Header = (props) => (
             <div className="inner">
         <img className="logo" src={tfv} alt="" />  
                 <h1>TEGUH</h1> 
-                <p>Fullstack Developer, Informatics Engineering Degree. <br/>Based in <a href="https://www.google.com/search?q=bali" target="_blank">Bali</a> Indonesia</p>
+                <p>i convert coffee to anything, Informatics Engineering Degree. <br/>Based in <a href="https://www.google.com/search?q=bali" target="_blank">Bali</a> Indonesia</p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="https://blogs-teguhfv.netlify.com/">Blog</a></li>
+                <li><a href="https://teguhfv-blog.netlify.app/">Blog</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
